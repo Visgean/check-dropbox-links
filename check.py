5f7b17805bf790da0f2d8fc17a1a618b39253c35
martin <Visgean@gmail.com>
2024-10-21 18:46:57 +0200

    black

--- check.py
+++ check.py
@@ -1,11 +1,11 @@
 import json
 
-with open('links.json') as f:
+with open("links.json") as f:
     links = json.loads(f.read())
 
 for link in links:
-    if link['link_permissions'].get('link_access_level', {}).get('.tag') == "editor":
-        print(link['name'], 'editable link')
+    if link["link_permissions"].get("link_access_level", {}).get(".tag") == "editor":
+        print(link["name"], "editable link")
 
 count = len(links)
-print(f'Checked {count} links')
+print(f"Checked {count} links")
--- download.py
+++ download.py
@@ -2,12 +2,12 @@ import json
 import os
 import requests
 
-token = os.environ.get('DROPBOX_TOKEN')
+token = os.environ.get("DROPBOX_TOKEN")
 
 if not token:
-    raise TypeError('give token')
+    raise TypeError("give token")
 
-URL = 'https://api.dropboxapi.com/2/sharing/list_shared_links'
+URL = "https://api.dropboxapi.com/2/sharing/list_shared_links"
 
 links = []
 cursor = None
@@ -15,28 +15,25 @@ cursor = None
 while True:
     params = {}
     if cursor:
-        params['cursor'] = cursor
+        params["cursor"] = cursor
 
     r = requests.post(
         URL,
         json=params,
-
         headers={
-            'Authorization': f'Bearer {token}',
-            "Content-Type": "application/json"
-        }
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        },
     )
 
     data = r.json()
-    links.extend(data['links'])
-    cursor = data.get('cursor')
+    links.extend(data["links"])
+    cursor = data.get("cursor")
 
-    if not data.get('has_more'):
+    if not data.get("has_more"):
         break
-
-
 
     print(data)
 
-with open('links.json', 'w') as f:
+with open("links.json", "w") as f:
     f.write(json.dumps(links, indent=2))
